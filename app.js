844fcc31d1911e639af1fbf76a5175fc8c220dbb
PINGWINbl4 <mikmez01@gmail.com>
2023-10-25 16:01:22 +0500

    first version

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const utils = require('./utils')
 const http = require('http')
+const createError = require('http-errors');
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+app.post('/', async (req, res, next) => {
+    res.json("chain geted")
+    console.log("chain geted")
+    try{
+        let timer = 0
+        const shelldue = req.body.shelldue
+        for (let i = 0; i < shelldue.ShellduesChainLink.length; i++) {
+            timer+=i*500
+            shelldue.ShellduesChainLink[i].action.timeout? 
+                        timer+= shelldue.ShellduesChainLink[i].action.timeout*1000:""
+            if(shelldue.ShellduesChainLink[i].action.set){
+                setTimeout(async() => {
+                    await utils.postToMQTT(shelldue.ShellduesChainLink[i].action)
+                },  timer);
+            }
+        }
+        shelldue.stage = 0
+        utils.updateShelldue(shelldue)
+    }
+    catch(err){
+        console.log(err)
+    }
+});
+
 app.use((req, res, next) => {
+    console.log(req.url, req.ip, req.method)
     next(createError.NotFound());
-  });
+});
   
 app.use((err, req, res, next) => {
 res.status(err.status || 500);
@@ -15,24 +42,6 @@ res.send({
     status: err.status || 500,
     message: err.message,
 });
-});
-
-app.post('/', async (req, res, next) => {
-    const shelldue = req.body.shelldue
-    switch (shelldue.chain[shelldue.stage]) {
-        case "timeout":
-            setTimeout(async() => {
-                shelldue.stage++
-                utils.updateShelldue(shelldue)
-            }, shelldue.chain[shelldue.stage].timeout);
-            break;
-        case "set":
-            
-            break;
-    
-        default:
-            break;
-    }
 });
 
 const PORT = process.env.APP_PORT || 5678;
--- utils.js
+++ utils.js
@@ -1,5 +1,4 @@
 const { PrismaClient } = require('@prisma/client');
-
 const db = new PrismaClient();
 
 async function updateShelldue(shelldue){
@@ -7,10 +6,45 @@ async function updateShelldue(shelldue){
         where:{
             id: shelldue.id
         },
-        data: shelldue
+        data: {
+            stage: shelldue.stage
+        }
     })
 }
 
+async function findSensorByElementId(elementId){
+    return await db.sensor.findFirst({
+        where:{
+            elementId:elementId
+        },
+        include:{
+            Station:true
+        }
+    })
+}
+
+async function postToMQTT(link){
+    const sensor = await findSensorByElementId(link.elementId)
+    const topic = `${sensor.Station.userId}/${sensor.Station.gatewayId}/${sensor.elementId}/set`
+    const postData = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          topic: topic,
+          shelldueScript: link.set
+        })
+      }
+    fetch(`http://${process.env.SHELLDUE_HOST}:${process.env.SHELLDUE_PORT}/`, postData)
+        .then(async (res) => {
+        console.log(await res.json())
+        })
+        .catch(err => {throw new Error(err)})
+}
+
 module.exports = {
-    updateShelldue
+    updateShelldue,
+    postToMQTT
 }
